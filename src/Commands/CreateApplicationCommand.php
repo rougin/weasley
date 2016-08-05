0102e11989a9d558bf57e2c52d36cbd78c669a86
Rougin Royce Gutib <rougingutib@gmail.com>
2016-08-06 02:35:03 +0800

    Update CreateApplicationCommand

--- src/Commands/CreateApplicationCommand.php
+++ src/Commands/CreateApplicationCommand.php
@@ -2,8 +2,6 @@
 
 namespace Rougin\Weasley\Commands;
 
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
