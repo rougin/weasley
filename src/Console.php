44db2ec1de28104ae31476370e90664da98cfc9d
Rougin Royce Gutib <rougingutib@gmail.com>
2018-04-13 14:24:33 +0800

    Update Console::VERSION

--- src/Console.php
+++ src/Console.php
@@ -12,7 +12,7 @@ use Symfony\Component\Console\Application;
  */
 class Console extends Application
 {
-    const VERSION = '0.6.0';
+    const VERSION = '0.6.1';
 
     /**
      * Instantiates the console application.
@@ -24,8 +24,11 @@ class Console extends Application
         parent::__construct('Weasley', $version);
 
         $this->add(new Commands\MakeControllerCommand);
+
         $this->add(new Commands\MakeIntegrationCommand);
+
         $this->add(new Commands\MakeMiddlewareCommand);
+
         $this->add(new Commands\MakeValidatorCommand);
     }
 }
