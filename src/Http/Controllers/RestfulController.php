f16a7436bf9175ca2a411f4d30f6cd9cf2b971e4
Rougin Gutib <rougingutib@gmail.com>
2019-09-20 19:56:37 +0800

    Update RestfulController

--- src/Http/Controllers/RestfulController.php
+++ src/Http/Controllers/RestfulController.php
@@ -7,7 +7,7 @@ namespace Rougin\Weasley\Http\Controllers;
  * NOTE: To be removed in v1.0.0. Use JsonController instead.
  *
  * @package Weasley
- * @author  Rougin Royce Gutib <rougingutib@gmail.com>
+ * @author  Rougin Gutib <rougingutib@gmail.com>
  */
 class RestfulController extends JsonController
 {
