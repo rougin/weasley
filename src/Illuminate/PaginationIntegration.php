4d93a7a2ab56703f6adbeef204fdb05d6d2dfadf
Rougin Gutib <rougingutib@gmail.com>
2019-09-28 22:46:07 +0800

    Update PaginationIntegration

--- src/Illuminate/PaginationIntegration.php
+++ src/Illuminate/PaginationIntegration.php
@@ -13,7 +13,7 @@ use Rougin\Slytherin\Integration\IntegrationInterface;
  * An integration for Laravel's Pagination package (illuminate/pagination).
  *
  * @package Weasley
- * @author  Rougin Royce Gutib <rougingutib@gmail.com>
+ * @author  Rougin Gutib <rougingutib@gmail.com>
  */
 class PaginationIntegration implements IntegrationInterface
 {
