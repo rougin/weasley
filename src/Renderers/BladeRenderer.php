9b173ea822a939e89d438fb643acc25c8dfdaeae
Rougin Gutib <rougingutib@gmail.com>
2019-10-09 18:00:27 +0800

    Update BladeRenderer

--- src/Renderers/BladeRenderer.php
+++ src/Renderers/BladeRenderer.php
@@ -11,7 +11,7 @@ use Rougin\Slytherin\Template\RendererInterface;
  * A simple wrapper of Laravel Blade to Slytherin's RendererInterface.
  *
  * @package Weasley
- * @author  Rougin Royce Gutib <rougingutib@gmail.com>
+ * @author  Rougin Gutib <rougingutib@gmail.com>
  */
 class BladeRenderer implements RendererInterface
 {
