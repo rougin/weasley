15ec7b261e77ee0682719accdf599ea25e8d6a6f
Rougin Gutib <rougingutib@gmail.com>
2019-10-23 20:59:14 +0800

    Update SessionTest

--- tests/Session/SessionTest.php
+++ tests/Session/SessionTest.php
@@ -8,7 +8,7 @@ use Rougin\Slytherin\Container\Container;
  * Session Test
  *
  * @package Weasley
- * @author  Rougin Royce Gutib <rougingutib@gmail.com>
+ * @author  Rougin Gutib <rougingutib@gmail.com>
  */
 class SessionTest extends AbstractTestCase
 {
