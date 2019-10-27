71ac7c7f2596b2863c3b998fc98f2a17958178eb
Rougin Gutib <rougingutib@gmail.com>
2019-10-27 23:01:13 +0800

    Update CheckPatchMethod

--- tests/Fixture/Middleware/CheckPatchMethod.php
+++ tests/Fixture/Middleware/CheckPatchMethod.php
@@ -10,7 +10,7 @@ use Psr\Http\Message\ServerRequestInterface;
  * "Check PATCH Method" Middleware
  *
  * @package Weasley
- * @author  Rougin Royce Gutib <rougingutib@gmail.com>
+ * @author  Rougin Gutib <rougingutib@gmail.com>
  */
 class CheckPatchMethod implements MiddlewareInterface
 {
