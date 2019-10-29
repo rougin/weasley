dbc2f6b405d3463e25a71af1bd579589745a1176
Rougin Gutib <rougingutib@gmail.com>
2019-10-29 23:02:17 +0800

    Update CheckTrimmedString

--- tests/Fixture/Middleware/CheckTrimmedString.php
+++ tests/Fixture/Middleware/CheckTrimmedString.php
@@ -10,7 +10,7 @@ use Psr\Http\Message\ServerRequestInterface;
  * "Check Trimmed String" Middleware
  *
  * @package Weasley
- * @author  Rougin Royce Gutib <rougingutib@gmail.com>
+ * @author  Rougin Gutib <rougingutib@gmail.com>
  */
 class CheckTrimmedString implements MiddlewareInterface
 {
