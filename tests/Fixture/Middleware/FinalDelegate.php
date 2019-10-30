05688ce3b1d818646b8c4ce6962a0887b834d21f
Rougin Gutib <rougingutib@gmail.com>
2019-10-30 23:02:47 +0800

    Update FinalDelegate

--- tests/Fixture/Middleware/FinalDelegate.php
+++ tests/Fixture/Middleware/FinalDelegate.php
@@ -10,7 +10,7 @@ use Psr\Http\Message\ServerRequestInterface;
  * Final Delegate
  *
  * @package Weasley
- * @author  Rougin Royce Gutib <rougingutib@gmail.com>
+ * @author  Rougin Gutib <rougingutib@gmail.com>
  */
 class FinalDelegate implements DelegateInterface
 {
