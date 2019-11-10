2f5ca46b6ab9d4f8d2bf123bd79da8dfc92d37b0
Rougin Gutib <rougingutib@gmail.com>
2019-11-10 14:44:41 +0800

    Update CrossOriginHeadersTest

--- tests/Middleware/CrossOriginHeadersTest.php
+++ tests/Middleware/CrossOriginHeadersTest.php
@@ -8,7 +8,7 @@ use Rougin\Weasley\Http\Middleware\Cors;
  * Cross Origin Headers Middleware Test
  *
  * @package Weasley
- * @author  Rougin Royce Gutib <rougingutib@gmail.com>
+ * @author  Rougin Gutib <rougingutib@gmail.com>
  */
 class CrossOriginHeadersTest extends AbstractTestCase
 {
