cc1517bd0eba5caacff412cf5a1f24bb5278726e
Rougin Gutib <rougingutib@gmail.com>
2019-11-11 14:44:56 +0800

    Update EmptyStringToNullTest

--- tests/Middleware/EmptyStringToNullTest.php
+++ tests/Middleware/EmptyStringToNullTest.php
@@ -8,7 +8,7 @@ use Rougin\Weasley\Fixture\Middleware\CheckQueryParams;
  * "Empty String To Null" Middleware Test
  *
  * @package Weasley
- * @author  Rougin Royce Gutib <rougingutib@gmail.com>
+ * @author  Rougin Gutib <rougingutib@gmail.com>
  */
 class EmptyStringToNullTest extends AbstractTestCase
 {
