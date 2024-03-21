2618bd2ee01ab24081cffa0ecc44cdf83f465f61
Rougin Gutib <rougingutib@gmail.com>
2024-03-21 16:20:29 +0800

    Add "Routes" directory, deprecate "Controllers" directory

--- src/Controllers/BaseController.php
+++ src/Controllers/BaseController.php
@@ -2,88 +2,16 @@
 
 namespace Rougin\Weasley\Controllers;
 
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
-use Rougin\Weasley\Transformer\JsonTransformer;
+use Rougin\Weasley\Routes\HttpRoute;
 
 /**
+ * @deprecated since ~0.7, use "HttpRoute" instead.
+ *
  * Base Controller
  *
  * @package Weasley
  * @author  Rougin Gutib <rougingutib@gmail.com>
  */
-class BaseController
+class BaseController extends HttpRoute
 {
-    /**
-     * @var \Psr\Http\Message\ServerRequestInterface
-     */
-    protected $request;
-
-    /**
-     * @var \Psr\Http\Message\ResponseInterface
-     */
-    protected $response;
-
-    /**
-     * Initializes the controller instance.
-     *
-     * @param \Psr\Http\Message\ServerRequestInterface $request
-     * @param \Psr\Http\Message\ResponseInterface      $response
-     */
-    public function __construct(ServerRequestInterface $request, ResponseInterface $response)
-    {
-        $parsed = $request->getParsedBody();
-
-        is_null($parsed) && $parsed = array();
-
-        $this->request = $request->withParsedBody($parsed);
-
-        $this->response = $response;
-    }
-
-    /**
-     * Sets the server request instance.
-     *
-     * @param  \Psr\Http\Message\ServerRequestInterface $request
-     * @return self
-     */
-    public function request(ServerRequestInterface $request)
-    {
-        $this->request = $request;
-
-        return $this;
-    }
-
-    /**
-     * Returns the specified data to JSON.
-     * NOTE: Must be moved to JsonController in v1.0.0.
-     * The visibility of this method must also be "protected".
-     *
-     * @param  mixed   $data
-     * @param  integer $code
-     * @param  integer $options
-     * @return \Psr\Http\Message\ResponseInterface
-     */
-    public function json($data, $code = 200, $options = 0)
-    {
-        $response = $this->response->withStatus($code);
-
-        $transformer = new JsonTransformer($response, $options);
-
-        return $transformer->transform($data);
-    }
-
-    /**
-     * Returns the specified data to JSON.
-     * NOTE: To be removed in v1.0.0. Use "json" method instead.
-     *
-     * @param  mixed   $data
-     * @param  integer $code
-     * @param  integer $options
-     * @return \Psr\Http\Message\ResponseInterface
-     */
-    public function toJson($data, $code = 200, $options = 0)
-    {
-        return $this->json($data, $code, $options);
-    }
 }
